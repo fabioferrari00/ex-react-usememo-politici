5060f58da2528dbb00782953f95d03c4777b1b7d
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-02 14:22:01 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useMemo } from "react"
 
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("")
 
   const loadUsers = async () => {
     const res = await fetch(`http://localhost:3333/politicians`)
@@ -14,12 +15,21 @@ function App() {
     loadUsers()
   }, [])
 
+  const filteredUsers = useMemo(() => {
+    return users.filter(user => {
+      const searchByName = user.name.toLowerCase().includes(search.toLowerCase());
+      const searchByBio = user.biography.toLowerCase().includes(search.toLowerCase());
+      return searchByName || searchByBio;
+    })
+  }, [users, search])
+
   return (
     <>
       <h1>Lista di politici</h1>
       <div className="container">
         <div className="row d-flex ">
-          {users.map((u, i) => {
+          <input type="text" name="" id="" value={search} onChange={e => setSearch(e.target.value)} placeholder="Cerca politico..." />
+          {filteredUsers.map((u, i) => {
             return (
               <div key={i} className="col-4">
                 <h3>{u.name}</h3>
